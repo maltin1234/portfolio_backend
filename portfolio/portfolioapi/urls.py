663c727c54fe9a03dc67ee9c873d21b8cec16748
Maltin Coku <matinimalmo@gmail.com>
2024-10-09 20:29:50 +0200

    stash

--- portfolio/portfolioapi/urls.py
+++ portfolio/portfolioapi/urls.py
@@ -1,40 +1,30 @@
-"""
-URL configuration for portfolioapi project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import re_path, include
+from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from users.views import UserViewSet
 
+# Initialize the router and register your viewset
+router = DefaultRouter()
+router.register('users', UserViewSet, basename='users')
 
+# Merged urlpatterns, making sure to include both sets of routes
 urlpatterns = [
-     re_path('admin/', admin.site.urls),
-     re_path('api-auth/', include('rest_framework.urls')),
-     re_path('api/todos', include('portfolioapp.urls')),
-     re_path('api/users', include('users.urls')),
-     re_path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-     re_path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path('admin/', admin.site.urls),
+    re_path('api-auth/', include('rest_framework.urls')),
+    re_path('api/todos/', include('portfolioapp.urls')),  # Don't forget to include the trailing slash
+    re_path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    re_path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # Include the router URLs for user-related views
+    re_path('', include(router.urls)),
+]
 
-  
-] 
-if settings.DEBUG: 
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
+# Serve media files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
